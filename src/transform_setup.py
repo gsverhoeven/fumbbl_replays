79edbfa377ab023861e86eae88eda94ede4dddf9
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-03-15 12:23:36 +0100

    move transform_setup to separate source file

---
+++ src/transform_setup.py
@@ -0,0 +1,29 @@
+def transform_setup(gamelog, df_roster, setup_id = 1):
+    res = gamelog.query("set_up_id == @setup_id")
+
+    res = res.sort_values('gameTime', ascending=False).drop_duplicates(['modelChangeKey']).sort_values('gameTime')
+
+    #loop over X-axis
+    # build up JSON
+    setup = ['setup']
+    for boardrow in range(12, -1, -1):
+        positions = []
+        tmp = res.query("PlayerCoordinateX == @boardrow")
+        for r in range(len(tmp)):
+            playerId = str(tmp.iloc[r]['modelChangeKey'])
+            positions.append(df_roster.query('playerId == @playerId')['short_name'].values[0] + ': ' + str(tmp.iloc[r]['modelChangeValue']))
+        #print(positions)
+        if len(tmp) > 0:
+            setup.append(positions)
+    
+    for boardrow in range(13, 26, 1):
+        positions = []
+        tmp = res.query("PlayerCoordinateX == @boardrow")
+        for r in range(len(tmp)):
+            playerId = str(tmp.iloc[r]['modelChangeKey'])
+            positions.append(df_roster.query('playerId == @playerId')['short_name'].values[0] + ': ' + str(tmp.iloc[r]['modelChangeValue']))
+        #print(positions)
+        if len(tmp) > 0:
+            setup.append(positions)
+
+    return setup
