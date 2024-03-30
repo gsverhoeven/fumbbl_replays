517f9538f93a7b69820a5b14ce4a8f405cf98a04
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-03-30 08:16:05 +0100

    prettify code

--- src/structure_player_actions.py
+++ src/structure_player_actions.py
@@ -21,13 +21,13 @@ def structure_player_actions(df):
             current_action = df.iloc[r]['modelChangeValue']['playerAction']
             current_active_player = df.iloc[r]['modelChangeValue']['actingPlayerId']
             df.iat[r, df.columns.get_loc('modelChangeKey')] = current_active_player
-            defenderId.append(0)
+            defenderId.append(0) 
             playerAction.append(current_action)
             if (current_action == "block"): 
-                keep.append(0)
+                keep.append(0) # not keeping these rows
             else:
                 keep.append(1)
-        elif (df.iloc[r]['modelChangeId'] == "block"): 
+        elif (df.iloc[r]['modelChangeId'] == "block"): # not keeping these rows
             df.iat[r, df.columns.get_loc('modelChangeKey')] = current_active_player
             playerAction.append(current_action)
             current_defender_id = df.iloc[r]['modelChangeValue']['defenderId']
@@ -64,7 +64,12 @@ def structure_player_actions(df):
     df['defenderId'] = defenderId
     df['keep2'] = keep      
 
-    # drop unnecessary rows
+    # drop unnecessary rows:
+    #
     df = df.query('keep2 == 1')
-    df = df.query('~(modelChangeId == "blockRoll" & playerAction == "block")') # choosingTeam can be deduced from player info (roster)
+    # choosingTeam can be deduced from player info (roster)
+    df = df.query('~(modelChangeId == "blockRoll" & playerAction == "block")') 
+    # deduce movement action from actual movement
+    df = df.query('~(modelChangeId == "playerAction" & playerAction == "move")') 
+
     return df
