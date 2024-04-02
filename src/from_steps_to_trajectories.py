d5096c69abca372864b33e370b3e17ca6dbbc132
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-04-02 23:23:59 +0200

    simplify trajectory function: use look ahead to detect trajectory ends

--- src/from_steps_to_trajectories.py
+++ src/from_steps_to_trajectories.py
@@ -6,57 +6,62 @@ def from_steps_to_trajectories(df):
     keep = []
 
     xy_mode = 0 # trajectory gather mode
-    keep_setball = 0
     active_player_id = "0"
     trajectory_id = 0
 
     for r in range(len(df)):
         if (df.iloc[r]['modelChangeId'] == "fieldModelSetPlayerCoordinate") \
-            & (df.iloc[r]['turnMode'] != "setup"):
+            & (df.iloc[r]['turnMode'] != "setup"): # grow trajectory
             if xy_mode == 0:
                 xy_mode = 1
-                path = []
-            if (active_player_id != df.iloc[r]['modelChangeKey']): 
+                active_player_id = df.iloc[r]['modelChangeKey']
                 trajectory_id += 1
-                trajectory[-1] = trajectory_id
+                trajectory[-1] = trajectory_id                
                 path = []
+            else: # xy_mode == 1
+                if (active_player_id != df.iloc[r]['modelChangeKey']): # switch naar nieuwe player
+                    active_player_id = df.iloc[r]['modelChangeKey']
+                    trajectory_id += 1
+                    trajectory[-1] = trajectory_id
+                    path = []
             # do stuff
             trajectory.append(trajectory_id)
             path.append(df.iloc[r]['modelChangeValue'])  
-            list_of_paths.append(path[:])          
-            active_player_id = df.iloc[r]['modelChangeKey']
-            keep.append(0)
-        elif (df.iloc[r]['modelChangeId'] == "fieldModelSetBallCoordinate"):
-            if(df.iloc[r+1]['modelChangeId'] == "fieldModelSetPlayerCoordinate"): # two step pattern matching: first check setball/setplayer combi
-                if(df.iloc[r]['modelChangeValue'] == df.iloc[r+1]['modelChangeValue']): # then check if new positions are the same
-                    keep.append(0)
-                    keep_setball = 0
-                else: # when positions differ we want to keep the setball as separate row
-                    keep.append(0)
-                    keep_setball = 1
+            list_of_paths.append(path[:])
+            # check for end of trajectory
+            if (df.iloc[r+1]['modelChangeId'] != "fieldModelSetPlayerCoordinate"):
+                if (df.iloc[r+1]['modelChangeId'] != "fieldModelSetBallCoordinate"):
+                    xy_mode = 0
+                    keep.append(1)
+                else:
+                    if (df.iloc[r+1]['modelChangeKey'] == active_player_id):
+                        keep.append(0)
+                    else:
+                        xy_mode = 0
+                        keep.append(1)
+            elif ( df.iloc[r+1]['modelChangeKey'] != active_player_id):
+                xy_mode = 0
+                keep.append(1)
             else:
                 keep.append(0)
-                keep_setball = 1
+        elif (df.iloc[r]['modelChangeId'] == "fieldModelSetBallCoordinate"): # BALL MOVES BEFORE PLAYER
+            if(df.iloc[r+1]['modelChangeId'] == "fieldModelSetPlayerCoordinate"): # two step pattern matching: first check setball/setplayer combi
+                if(str(df.iloc[r]['modelChangeValue']) == str(df.iloc[r+1]['modelChangeValue'])): # then check if new positions are the same
+                    keep.append(0)
+                else: # when positions differ we want to keep the setball as separate row
+                    keep.append(1)
+            else:
+                keep.append(0)
             trajectory.append(-1)    
             list_of_paths.append(path[:])                                    
         else:
-            if(xy_mode == 1):
-                keep.append(1)
-            elif(keep_setball == 1):
-                keep.append(1)
-                keep_setball = 0
-            else:
-                keep.append(0)
-            xy_mode = 0
-            #print(path)
+            keep.append(0)
             path = []
             trajectory.append(-1)
             list_of_paths.append(path[:]) # by value, not by reference
 
     df['list_of_paths'] = list_of_paths
-    keep.append(0) 
-    df['keep'] = keep[1:] # shift all elements up to align with record we want to keep          
-    
+    df['keep'] = keep
     # drop unnecessary move rows
     df = df.query('~(turnMode == "regular" & modelChangeId == "fieldModelSetPlayerCoordinate" & keep == 0)')
     df = df.query('~(turnMode == "regular" & modelChangeId == "fieldModelSetBallCoordinate" & keep == 0)')
