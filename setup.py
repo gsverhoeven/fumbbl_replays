da248372436852518140180d5d79c38cae83ec0f
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-04-28 14:23:51 +0200

    convert scripts to Python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='fumbbl_bbgn',
+      version='0.0.1',
+      description='Convert FUMBBL replay to BBGN format',
+      url='https://github.com/gsverhoeven/fumbbl_replays',
+      author='Gertjan Verhoeven',
+      packages=['fumbbl_bbgn'],
+      author_email='gertjan.verhoeven@gmail.com',
+      license='MIT License',
+      install_requires=['pandas'],
+)
+
+
