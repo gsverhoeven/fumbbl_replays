566c070648c2a82bf1a75ef792ff90ae49f7b19d
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-05-09 09:51:51 +0200

    expose more functions to users

--- fumbbl_replays/__init__.py
+++ fumbbl_replays/__init__.py
@@ -1,4 +1,8 @@
 from .fumbbl2bbgn import fumbbl2bbgn
-from .fetch_replay import fetch_replay
 
-# PM here we also expose the board plotting functions
+from .fetch_replay import fetch_replay
+from .parse_replay import parse_replay
+from .extract_players_from_replay import extract_players_from_replay
+from .extract_rosters_from_replay import extract_rosters_from_replay
+
+from .kickoff_formations import create_plot, determine_receiving_team_at_start, sort_defensive_plots
