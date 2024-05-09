93c1ad662fb7ce16aa5ded4a3f02be2f0cfc218f
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-05-09 12:43:40 +0200

    do not expose extract_players: internal function

--- fumbbl_replays/__init__.py
+++ fumbbl_replays/__init__.py
@@ -2,7 +2,6 @@ from .fumbbl2bbgn import fumbbl2bbgn
 
 from .fetch_replay import fetch_replay
 from .parse_replay import parse_replay
-from .extract_players_from_replay import extract_players_from_replay
 from .extract_rosters_from_replay import extract_rosters_from_replay
 
 from .kickoff_formations import create_plot, determine_receiving_team_at_start, sort_defensive_plots
