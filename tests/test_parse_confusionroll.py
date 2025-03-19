5f9b93bbb97d87c1dfd4d93c1f16ab4e8f44f2a2
Gertjan <gertjan.verhoeven@gmail.com>
2025-03-19 20:42:20 +0100

    Create test_parse_confusionroll.py

---
+++ tests/test_parse_confusionroll.py
@@ -0,0 +1,11 @@
+from fumbbl_replays.parse_confusionroll import parse_confusionroll
+import json
+
+obj = '{"reportId": "confusionRoll", "playerId": "15064977", "successful": true, "roll": 2, "minimumRoll": 2, "reRolled": false, "confusionSkill": "Bone Head"}'
+
+obj = json.loads(obj)
+out = parse_confusionroll(obj)
+
+desired_out = 'Confusion roll: 2 | 15064977 acts normally'
+
+assert out == desired_out
