5669735a9007ec77f3b44acdc3c670775f483928
Gertjan <gertjan.verhoeven@gmail.com>
2025-03-29 01:04:19 +0100

    switch from factor to integers for tile coordinates

--- src/fumbbl_replays/plot_team_league_development.py
+++ src/fumbbl_replays/plot_team_league_development.py
@@ -34,32 +34,38 @@ def fetch_team_development_data(team_id, n_matches = 15):
             res = pd.concat([res, df_positions])
         i = i + 1
 
-    cell_color = []
     first_skill = []
     second_skill = []
+    first_skill_color = []
+    second_skill_color = []
 
     for i in range(len(res)):
         n_skills = len(res.iloc[i]['learned_skills'])
         if n_skills == 0:
-            cell_color.append("grey")
             first_skill.append("-")
             second_skill.append("-")
+            first_skill_color.append("grey")
+            second_skill_color.append("NA")            
         elif n_skills == 1:
-            cell_color.append(res.iloc[i]['skill_colors'][n_skills - 1])
             first_skill.append(res.iloc[i]['learned_skills'][n_skills - 1])
             second_skill.append("-")
+            first_skill_color.append(res.iloc[i]['skill_colors'][n_skills - 1])
+            second_skill_color.append("NA")          
         elif n_skills == 2:
-            cell_color.append(res.iloc[i]['skill_colors'][n_skills])
-            first_skill.append(res.iloc[i]['learned_skills'][n_skills])
-            second_skill.append("-")            
+            first_skill.append(res.iloc[i]['learned_skills'][n_skills - 2])
+            second_skill.append(res.iloc[i]['learned_skills'][n_skills - 1])
+            first_skill_color.append(res.iloc[i]['skill_colors'][n_skills - 2])
+            second_skill_color.append(res.iloc[i]['skill_colors'][n_skills - 1])            
         else:
-            cell_color.append("black")
             first_skill.append(">2")
-            second_skill.append(">2") 
+            second_skill.append(">2")
+            first_skill_color.append("black")
+            second_skill_color.append("black")            
 
-    res['cell_color'] = cell_color
     res['first_skill'] = first_skill
     res['second_skill'] = second_skill
+    res['first_skill_color'] = first_skill_color
+    res['second_skill_color'] = second_skill_color
     res = res.drop(columns = ['short_name'])
 
     res = res.query('first_skill != "Loner" & recoveringInjury == "None" ')
@@ -68,7 +74,7 @@ def fetch_team_development_data(team_id, n_matches = 15):
     roster = fetch_roster(rosterName).filter(['positionName', 'shorthand'])
 
     player_list = (res
-        .groupby(['playerName', 'positionName'], as_index = False)
+        .groupby(['playerName', 'positionName', 'cost'], as_index = False)
         .agg(count = ('index', 'count'), first_match = ('match_count', 'min'))
         .merge(roster, on = 'positionName')
         .sort_values(['positionName', 'first_match'])
@@ -77,17 +83,22 @@ def fetch_team_development_data(team_id, n_matches = 15):
     player_list['positionNr'] = player_list.groupby(['positionName']).cumcount() + 1
     # create shorthand id
     player_list['short_name'] = player_list['shorthand'] + player_list['positionNr'].astype(str)
+    
+    player_list = player_list.sort_values(['cost', 'first_match'])
+    player_list['plotPosition'] = range(1, len(player_list) + 1)
 
-    res = res.merge(player_list.filter(['playerName', 'shorthand', 'short_name', 'positionNr']), on = 'playerName')
+    res = res.merge(player_list.filter(['playerName', 'shorthand', 'short_name', 'positionNr', 'plotPosition']), on = 'playerName')
     return res
 
 def make_team_development_plot(res):
     rosterName = res['rosterName'].unique()[0]
     p = (
-        ggplot(res, aes(x="factor(match_count)", y="reorder(short_name, cost)"))
-        + geom_tile(aes(fill = "cell_color", width=0.95, height=0.95))
-        + geom_text(aes(label="first_skill"), size=6)
-        + scale_fill_identity()
+        ggplot(res)
+        + geom_tile(aes(x="match_count", y="plotPosition", fill = "first_skill_color"), width=0.95, height=0.95)
+        + geom_tile(data = res.query('second_skill_color != "NA"'), mapping = aes(x="match_count+0.5", y="plotPosition-0.5", fill = "second_skill_color"), width=0.95, height=0.95)
+        + geom_text(aes(x="match_count", y="plotPosition", label="first_skill"), size=6)
+        + geom_text(data = res.query('second_skill_color != "NA"'), mapping = aes(x="match_count+0.5", y="plotPosition-0.5", label="second_skill"), size = 6)
+        + scale_fill_identity(na_value = "NA")
         #+ coord_equal(expand=False)  # new
         + theme(figure_size=(12, 6))  # new
         + theme(legend_position='none')
