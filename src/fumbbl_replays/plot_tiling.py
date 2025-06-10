ba009ed45a272b9bbd52da9eec1b1295fea5fbad
Gertjan <gertjan.verhoeven@gmail.com>
2025-06-10 21:24:43 +0200

    add scale resizing argument

--- src/fumbbl_replays/plot_tiling.py
+++ src/fumbbl_replays/plot_tiling.py
@@ -19,7 +19,7 @@ def select_images(df_replays, race_name_defense, race_name_offense_list):
                     im_list.append(my_im)
     return im_list
 
-def make_tiling(im_list, h = 2):
+def make_tiling(im_list, h = 2,  scale = 1.0):
     im1 = im_list[0]
     v = ceil(len(im_list) / h)
     dst = Image.new(mode = 'RGB', size = (im1.width * h, im1.height * v),  color = (153, 153, 153))
@@ -31,4 +31,7 @@ def make_tiling(im_list, h = 2):
             else:
                 dst.paste(im_list[n_im], (im1.width * hc , im1.height * vc))
             n_im = n_im + 1
+    
+    dst = dst.resize(size = (ceil(dst.width * scale), ceil(dst.height * scale)) )
+
     return display(dst)
